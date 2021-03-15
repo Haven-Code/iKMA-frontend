fe2cd74bd653b27aa123b7b6c2879982ec4a7eca
Dang Hoang Phuc <13364457+phuchptty@users.noreply.github.com>
2021-03-16 01:59:10 +0700

    Update Calendar.vue

--- src/views/Dashboard/Calendar.vue
+++ src/views/Dashboard/Calendar.vue
@@ -32,36 +32,35 @@
 			<v-flex fill-height>
 				<v-calendar ref="calendar" v-model="webViewCalendar.focus" @click:event="showEvent" @click:more="viewDay" :events="webViewCalendar.events" color="primary" class="ppx-border-0"></v-calendar>
 
-				<v-menu v-model="webViewCalendar.selectedOpen" :activator="webViewCalendar.selectedElement" :close-on-content-click="false" offset-x>
-					<v-card color="grey darken-2" style="opacity: 0.95" min-width="40vw" flat>
+				<v-menu v-if="webViewCalendar.selectedOpen" v-model="webViewCalendar.selectedOpen" :activator="webViewCalendar.selectedElement" :close-on-content-click="false" offset-x>
+					<v-card color="grey darken-1" style="opacity: 0.95" min-width="450px" flat>
 						<v-toolbar dark color="primary">
 							<v-toolbar-title v-html="webViewCalendar.selectedEvent.name"></v-toolbar-title>
 							<v-spacer></v-spacer>
 						</v-toolbar>
 
-						<v-card-text class="pb-0 ppx-text-white">
-							<!-- <span v-html="webViewCalendar.selectedEvent.details"></span> -->
-							<p>
+						<v-card-text class="pb-0 ppx-text-white ppx-text-lg">
+							<p class="ma-0 pa-0">
 								Môn:
 								<strong>{{ webViewCalendar.selectedEvent.eventData.subjectName }} ({{ webViewCalendar.selectedEvent.eventData.subjectCode }})</strong>
 							</p>
 
-							<p>
+							<p class="ma-0 pa-0">
 								Lớp:
-								<strong>{{webViewCalendar.selectedEvent.eventData.className}}</strong>
+								<strong>{{ webViewCalendar.selectedEvent.eventData.className }}</strong>
 							</p>
 
-							<p>
+							<p class="ma-0 pa-0">
 								Tiết:
 								<strong>{{ webViewCalendar.selectedEvent.eventData.lesson }}</strong>
 							</p>
 
-							<p>
+							<p class="ma-0 pa-0">
 								Phòng Học:
 								<strong>{{ webViewCalendar.selectedEvent.eventData.room }}</strong>
 							</p>
 
-							<p>
+							<p class="ma-0 pa-0">
 								Giáo Viên:
 								<strong>{{ webViewCalendar.selectedEvent.eventData.teacher }}</strong>
 							</p>
@@ -81,163 +80,168 @@
 </template>
 
 <script>
-	import Navbar from '../../components/Navbar.vue'
-	import { mapState } from 'vuex'
-	import dayjs from 'dayjs'
-	import customParseFormat from 'dayjs/plugin/customParseFormat'
-	dayjs.extend(customParseFormat)
-	import 'dayjs/locale/vi'
-	dayjs.locale('vi')
+import Navbar from '../../components/Navbar.vue';
+import { mapState } from 'vuex';
+import dayjs from 'dayjs';
+import customParseFormat from 'dayjs/plugin/customParseFormat';
+dayjs.extend(customParseFormat);
+import 'dayjs/locale/vi';
+dayjs.locale('vi');
 
-	export default {
-		name: 'Calendar',
-		components: {
-			Navbar,
+export default {
+	name: 'Calendar',
+	components: {
+		Navbar,
+	},
+	data: () => ({
+		webViewCalendar: {
+			events: [],
+			focus: '',
+			selectedEvent: {},
+			selectedElement: null,
+			selectedOpen: false,
+			type: ''
 		},
-		data: () => ({
-			webViewCalendar: {
-				events: [],
-				focus: '',
-				selectedEvent: {},
-				selectedElement: null,
-				selectedOpen: false,
-			},
-		}),
-		computed: {
-			...mapState(['user']),
+	}),
+	computed: {
+		...mapState(['user']),
+	},
+	methods: {
+		setToday() {
+			this.webViewCalendar.focus = '';
 		},
-		methods: {
-			setToday() {
-				this.webViewCalendar.focus = ''
-			},
-			prev() {
-				this.$refs.calendar.prev()
-			},
-			next() {
-				this.$refs.calendar.next()
-			},
-			showEvent({ nativeEvent, event }) {
-				const open = () => {
-					this.webViewCalendar.selectedEvent = event
-					this.webViewCalendar.selectedElement = nativeEvent.target
-					setTimeout(() => {
-						this.webViewCalendar.selectedOpen = true
-					}, 10)
-				}
+		viewDay({ date }) {
+			this.webViewCalendar.focus = date;
+			this.webViewCalendar.type = 'day';
+		},
+		prev() {
+			this.$refs.calendar.prev();
+		},
+		next() {
+			this.$refs.calendar.next();
+		},
+		showEvent({ nativeEvent, event }) {
+			const open = () => {
+				this.webViewCalendar.selectedEvent = event;
+				this.webViewCalendar.selectedElement = nativeEvent.target;
+				setTimeout(() => {
+					this.webViewCalendar.selectedOpen = true;
+				}, 10);
+			};
 
-				if (this.webViewCalendar.selectedOpen) {
-					this.webViewCalendar.selectedOpen = false
-					setTimeout(open, 10)
-				} else {
-					open()
-				}
+			if (this.webViewCalendar.selectedOpen) {
+				this.webViewCalendar.selectedOpen = false;
+				setTimeout(open, 10);
+			} else {
+				open();
+			}
 
-				nativeEvent.stopPropagation()
-			},
-			translateMonth(str) {
-				let a = str.split(' ')
-				const dic = {
-					January: 'Tháng Một',
-					February: 'Tháng Hai',
-					March: 'Tháng Ba',
-					April: 'Thánng Bốn',
-					May: 'Tháng Năm',
-					June: 'Tháng Sáu',
-					July: 'Tháng Bảy',
-					August: 'Tháng Tám',
-					September: 'Tháng Chín',
-					October: 'Tháng Mười',
-					November: 'Tháng Mười Một',
-					December: 'Tháng Mười Hai',
-				}
-				let text = dic[a[0]] + ' ' + a[1]
-				return text
-			},
-			convertLessonsToTime(lessons) {
-				let time = {
-					start: '',
-					end: '',
-				}
-				switch (lessons) {
-					case '1,2,3':
-						time = {
-							start: '7:00',
-							end: '9:25',
-						}
-						break
-					case '4,5,6':
-						time = {
-							start: '9:35',
-							end: '12:00',
-						}
-						break
-					case '7,8,9':
-						time = {
-							start: '12:30',
-							end: '14:55',
-						}
-						break
-					case '10,11,12':
-						time = {
-							start: '15:05',
-							end: '17:30',
-						}
-						break
-					case '13,14,15,16':
-						time = {
-							start: '18:00',
-							end: '21:15',
-						}
-						break
-					case '7,8,9,10':
-						time = {
-							start: '12:30',
-							end: '15:50',
-						}
-						break
-				}
-				return time
-			},
-			convertDayTime(day, lesson) {
-				let lessonTime = this.convertLessonsToTime(lesson)
-				let time = {
-					start: '',
-					end: '',
-				}
-				if (lessonTime.start == '' || lessonTime.end == '') {
+			nativeEvent.stopPropagation();
+		},
+		translateMonth(str) {
+			let a = str.split(' ');
+			const dic = {
+				January: 'Tháng Một',
+				February: 'Tháng Hai',
+				March: 'Tháng Ba',
+				April: 'Thánng Bốn',
+				May: 'Tháng Năm',
+				June: 'Tháng Sáu',
+				July: 'Tháng Bảy',
+				August: 'Tháng Tám',
+				September: 'Tháng Chín',
+				October: 'Tháng Mười',
+				November: 'Tháng Mười Một',
+				December: 'Tháng Mười Hai',
+			};
+			let text = dic[a[0]] + ' ' + a[1];
+			return text;
+		},
+		convertLessonsToTime(lessons) {
+			let time = {
+				start: '',
+				end: '',
+			};
+			switch (lessons) {
+				case '1,2,3':
 					time = {
-						start: dayjs(day, 'DD/MM/YYYY').format('YYYY-MM-DD'),
-						end: dayjs(day, 'DD/MM/YYYY').format('YYYY-MM-DD'),
-					}
-				} else {
+						start: '7:00',
+						end: '9:25',
+					};
+					break;
+				case '4,5,6':
 					time = {
-						start: dayjs((day + ' ' + lessonTime.start).trim(), 'DD/MM/YYYY H:mm').format('YYYY-MM-DD HH:mm'),
-						end: dayjs((day + ' ' + lessonTime.end).trim(), 'DD/MM/YYYY H:mm').format('YYYY-MM-DD HH:mm'),
-					}
-				}
-				return time
-			},
-			addEvent() {
-				const { userSchedule } = this.user
-
-				this.webViewCalendar.events = userSchedule.map((x) => {
-					const { start, end } = this.convertDayTime(x.day, x.lesson)
-
-					return {
-						name: x.subjectName,
-						eventData: x,
-						start: start,
-						end: end,
-						color: 'blue lighten-1',
-					}
-				})
-			},
+						start: '9:35',
+						end: '12:00',
+					};
+					break;
+				case '7,8,9':
+					time = {
+						start: '12:30',
+						end: '14:55',
+					};
+					break;
+				case '10,11,12':
+					time = {
+						start: '15:05',
+						end: '17:30',
+					};
+					break;
+				case '13,14,15,16':
+					time = {
+						start: '18:00',
+						end: '21:15',
+					};
+					break;
+				case '7,8,9,10':
+					time = {
+						start: '12:30',
+						end: '15:50',
+					};
+					break;
+			}
+			return time;
 		},
-		mounted() {
-			this.setToday()
-			this.addEvent()
+		convertDayTime(day, lesson) {
+			let lessonTime = this.convertLessonsToTime(lesson);
+			let time = {
+				start: '',
+				end: '',
+			};
+			if (lessonTime.start == '' || lessonTime.end == '') {
+				time = {
+					start: dayjs(day, 'DD/MM/YYYY').format('YYYY-MM-DD'),
+					end: dayjs(day, 'DD/MM/YYYY').format('YYYY-MM-DD'),
+				};
+			} else {
+				time = {
+					start: dayjs((day + ' ' + lessonTime.start).trim(), 'DD/MM/YYYY H:mm').format('YYYY-MM-DD HH:mm'),
+					end: dayjs((day + ' ' + lessonTime.end).trim(), 'DD/MM/YYYY H:mm').format('YYYY-MM-DD HH:mm'),
+				};
+			}
+			return time;
 		},
-	}
+		addEvent() {
+			const { userSchedule } = this.user;
+
+			this.webViewCalendar.events = userSchedule.map((x) => {
+				const { start, end } = this.convertDayTime(x.day, x.lesson);
+
+				return {
+					name: x.subjectName,
+					eventData: x,
+					start: start,
+					end: end,
+					color: 'blue lighten-1',
+				};
+			});
+		},
+	},
+	mounted() {
+		this.setToday();
+		this.addEvent();
+	},
+};
 </script>
 
 <style></style>

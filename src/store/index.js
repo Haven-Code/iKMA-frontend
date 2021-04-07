b2859625fafa049771622dfca63408d6b6dc6522
Phuc Dang <phuc.px.371@gmail.com>
2021-04-07 13:10:31 +0700

    fix blank page

--- src/store/index.js
+++ src/store/index.js
@@ -1,44 +1,44 @@
 /** @format */
 
-import Vue from 'vue';
-import Vuex from 'vuex';
-import createPersistedState from 'vuex-persistedstate';
-import SecureLS from 'secure-ls';
-import i18n, { selectedLocale } from '@/plugins/i18n';
+import Vue from 'vue'
+import Vuex from 'vuex'
+import createPersistedState from 'vuex-persistedstate'
+import SecureLS from 'secure-ls'
+import i18n, { selectedLocale } from '@/plugins/i18n'
 
-import user from './user';
+import user from './user'
 
 const ls = new SecureLS({
 	isCompression: true,
 	encodingType: 'aes',
 	encryptionSecret: process.env.VUE_APP_STORAGE_KEY,
-});
+})
 
 // const ls = new SecureLS({
 // 	isCompression: false,
 // });
 
-Vue.use(Vuex);
+Vue.use(Vuex)
 
-const getItemFromKey = (key) => {
+const getItemFromKey = (lsSec, key) => {
 	try {
-		let a = ls.get(key);
-		return a;
+		return lsSec.get(key)
 	} catch (e) {
 		//console.log(e);
-		console.error("WRONG KEY ! REDIRECT TO AUTH #PPX")
-		ls.removeAll();
+		console.error('WRONG KEY ! REDIRECT TO AUTH #PPX')
+		lsSec.removeAll()
+		window.location = '/auth'
 	}
-};
+}
 
 const pluginState = createPersistedState({
 	storage: {
-		getItem: (key) => getItemFromKey(key),
+		getItem: (key) => getItemFromKey(ls, key),
 		setItem: (key, value) => ls.set(key, value),
 		removeItem: (key) => ls.remove(key),
 	},
 	paths: ['user', 'config'],
-});
+})
 
 export default new Vuex.Store({
 	state: {
@@ -50,36 +50,36 @@ export default new Vuex.Store({
 	},
 	mutations: {
 		SET_CONFIG: (state, payload) => {
-			state.config = payload;
+			state.config = payload
 		},
 		DRAWER: (state) => {
-			let drawer = state.config.drawer;
-			state.config.drawer = !drawer;
+			let drawer = state.config.drawer
+			state.config.drawer = !drawer
 		},
 		SET_THEME: (state, payload) => {
-			state.config.darkMode = payload;
+			state.config.darkMode = payload
 		},
 		SET_LANG: (state, payload) => {
-			state.config.lang = payload;
+			state.config.lang = payload
 		},
 	},
 	actions: {
 		setConfig: ({ commit }, payload) => {
-			commit('SET_CONFIG', payload);
+			commit('SET_CONFIG', payload)
 		},
 		toggleDrawer: ({ commit }) => {
-			commit('DRAWER');
+			commit('DRAWER')
 		},
 		toggleDarkMode: ({ commit }, payload) => {
-			commit('SET_THEME', payload);
+			commit('SET_THEME', payload)
 		},
 		changeLanguage: ({ commit }, payload) => {
-			i18n.locale = payload;
-			commit('SET_LANG', payload);
+			i18n.locale = payload
+			commit('SET_LANG', payload)
 		},
 	},
 	modules: {
 		user: user,
 	},
 	plugins: [pluginState],
-});
+})
